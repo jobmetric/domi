79717901b30e81f75ed549da2634a269098d0665
majid mohammadian <majeedmohammadian@gmail.com>
2023-12-29 22:00:51 +0330

    Service provider update based on PackageCore, but not completed yet

--- src/DomiServiceProvider.php
+++ src/DomiServiceProvider.php
@@ -5,37 +5,41 @@ namespace JobMetric\Domi;
 use Illuminate\Support\ServiceProvider;
 use JobMetric\Domi\Console\Commands\CreateViewCommand;
 use JobMetric\Domi\Providers\EventServiceProvider;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\Exceptions\ViewFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
 
-class DomiServiceProvider extends ServiceProvider
+class DomiServiceProvider extends PackageCoreServiceProvider
 {
-    public function register(): void
+    /**
+     * @throws RegisterClassTypeNotFoundException
+     * @throws ViewFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
     {
-        $this->app->bind('Domi', function ($app) {
-            return new Domi($app);
-        });
-
-        $this->app->register(EventServiceProvider::class);
-
-        // merge config
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'domi');
-
-        // load views
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'domi');
-
-        // load translations
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'domi');
+        $package->name('domi')
+            ->hasConfig()
+            ->hasTranslation()
+            ->hasView()
+            ->registerCommand(CreateViewCommand::class)
+            ->registerClass('Domi', Domi::class);
     }
+
+    /*public function register(): void
+    {
+        $this->app->register(EventServiceProvider::class);
+    }*/
 
     /**
      * boot provider
      *
      * @return void
      */
-    public function boot(): void
+    /*public function boot(): void
     {
         $this->registerPublishables();
-        $this->registerCommands();
-    }
+    }*/
 
     /**
      * register publishables
@@ -48,37 +52,9 @@ class DomiServiceProvider extends ServiceProvider
             return;
         }
 
-        // publish config
-        $this->publishes([
-            realpath(__DIR__ . '/../config/config.php') => config_path('domi.php')
-        ], 'config');
-
         // publish assets
         $this->publishes([
             realpath(__DIR__ . '/../assets') => public_path('vendor/domi')
         ], 'public');
-
-        // publish views
-        $this->publishes([
-            realpath(__DIR__ . '/../resources/views') => resource_path('views/vendor/domi')
-        ], 'views');
-    }
-
-    /**
-     * Register Commands
-     *
-     * @return void
-     */
-    private function registerCommands(): void
-    {
-        if (!$this->app->runningInConsole()) {
-            return;
-        }
-
-        $this->app->bind('command.domi:view', CreateViewCommand::class);
-
-        $this->commands([
-            'command.domi:view'
-        ]);
     }
 }
