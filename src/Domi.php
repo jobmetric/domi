ec9c2f75f2ea024cce40a806de0f49bd3b052e92
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-17 01:29:26 +0330

    remove attribute in service

--- src/Domi.php
+++ src/Domi.php
@@ -13,28 +13,6 @@ class Domi
      */
     protected Application $app;
 
-    private static Domi $instance;
-
-    private ?string $logo = null;
-    private ?string $favicon = null;
-    private ?string $theme_color = null;
-    private ?string $background_color = null;
-    private ?string $section = null;
-    private ?string $title = null;
-    private ?string $description = null;
-    private ?string $keywords = null;
-    private ?string $canonical = null;
-    private ?string $image = null;
-    private ?string $page_type = null;
-    private ?string $robots = 'index,follow';
-    private ?string $body_class = null;
-    private array $plugins = [];
-    private array $localize = [];
-    private array $localize_counter = [];
-    private array $links = [];
-    private array $styles = [];
-    private array $scripts = [];
-
     /**
      * Create a new Domi instance.
      *
