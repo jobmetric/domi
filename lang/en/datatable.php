22f4475bbff741dc5a89c925e4d899c0529d00d2
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-17 01:26:39 +0330

    change language file

---
+++ lang/en/datatable.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Datatable Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'processing'        => 'Loading...',
+    'search'            => 'Search: ',
+    'lengthMenu'        => 'Number : _MENU_',
+    'info'              => 'Count _START_ to _END_ from _TOTAL_',
+    'infoEmpty'         => 'There is nothing to show',
+    'infoFiltered'      => '',
+    'infoPostFix'       => '',
+    'loadingRecords'    => 'Loading...',
+    'zeroRecords'       => 'Your search returned no results',
+    'emptyTable'        => 'No data available',
+    'paginate_first'    => '<<',
+    'paginate_previous' => '<',
+    'paginate_next'     => '>',
+    'paginate_last'     => '>>'
+
+];
---
+++ lang/en/sweetalert.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sweetalert Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'title'  => [
+        'warning'    => 'Warning',
+        'attention'  => 'Attention',
+        'permission' => 'Inaccessibility',
+    ],
+    'button' => [
+        'got_it'   => 'Got in',
+        'realized' => 'Realized',
+        'ok'       => 'Ok',
+        'cancel'   => 'Cancel',
+    ],
+    'option' => [
+        'timer'      => '2500',
+        'background' => 'white',
+        'position'   => [
+            'center' => 'top-center',
+            'end'    => 'top-end',
+        ],
+    ],
+    'text'   => [
+        'delete_record'         => 'Do you want to delete this record?',
+        'success_delete_record' => 'The record was successfully deleted, click the save button to finish.',
+    ],
+
+];
