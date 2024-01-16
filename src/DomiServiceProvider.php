8fd60d96f4d603f19924448aee28ef2b4f0d9195
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-17 00:38:10 +0330

    change provider based on package core version 1.5

--- src/DomiServiceProvider.php
+++ src/DomiServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace JobMetric\Domi;
 
-use Illuminate\Support\ServiceProvider;
 use JobMetric\Domi\Console\Commands\CreateViewCommand;
-use JobMetric\Domi\Providers\EventServiceProvider;
+use JobMetric\PackageCore\Exceptions\AssetFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
 use JobMetric\PackageCore\Exceptions\ViewFolderNotFoundException;
 use JobMetric\PackageCore\PackageCore;
@@ -15,6 +14,7 @@ class DomiServiceProvider extends PackageCoreServiceProvider
     /**
      * @throws RegisterClassTypeNotFoundException
      * @throws ViewFolderNotFoundException
+     * @throws AssetFolderNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
@@ -22,39 +22,8 @@ class DomiServiceProvider extends PackageCoreServiceProvider
             ->hasConfig()
             ->hasTranslation()
             ->hasView()
+            ->hasAsset()
             ->registerCommand(CreateViewCommand::class)
             ->registerClass('Domi', Domi::class);
-    }
-
-    /*public function register(): void
-    {
-        $this->app->register(EventServiceProvider::class);
-    }*/
-
-    /**
-     * boot provider
-     *
-     * @return void
-     */
-    /*public function boot(): void
-    {
-        $this->registerPublishables();
-    }*/
-
-    /**
-     * register publishables
-     *
-     * @return void
-     */
-    protected function registerPublishables(): void
-    {
-        if (!$this->app->runningInConsole()) {
-            return;
-        }
-
-        // publish assets
-        $this->publishes([
-            realpath(__DIR__ . '/../assets') => public_path('vendor/domi')
-        ], 'public');
     }
 }
