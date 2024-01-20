d326264001422e872eda805fb063f121b7c9227f
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-20 23:26:07 +0330

    change event

--- src/Events/InitDomiEvent.php
+++ src/Events/InitDomiEvent.php
@@ -2,25 +2,6 @@
 
 namespace JobMetric\Domi\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
 class InitDomiEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, Channel>
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('DomiInit'),
-        ];
-    }
 }
