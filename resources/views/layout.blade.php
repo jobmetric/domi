ea6820220dde72056ee5da304e97b74422622460
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-22 21:48:47 +0330

    add some method in layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ trans('domi:base.lang') }}"@if(trans('domi:base.direction') == 'rtl') direction="rtl" style="direction: rtl;"@endif>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="{{ env('app_url') }}"/>
+    <title>@domi('title')</title>
+    <meta name="description" content="@domi('description')">
+    <meta name="keywords" content="@domi('keywords')">
+    <meta name="author" content="@domi('author')">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     {{--{!! Domi::document()->getHeader() !!}--}}
     @yield('style')
 
