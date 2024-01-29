c2c1d62490014c07cbade7abbd8b04995da84399
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-29 22:18:43 +0330

    remove set word in all helper

--- src/helpers.php
+++ src/helpers.php
@@ -3,85 +3,85 @@
 use JobMetric\Domi\Enums\PageTypeEnum;
 use JobMetric\Domi\Facades\Domi;
 
-if (!function_exists('DomiSetTitle')) {
+if (!function_exists('DomiTitle')) {
     /**
      * set title for title tag
      *
      * @param string $title
      * @return void
      */
-    function DomiSetTitle(string $title): void
+    function DomiTitle(string $title): void
     {
         Domi::setTitle($title);
     }
 }
 
-if (!function_exists('DomiSetDescription')) {
+if (!function_exists('DomiDescription')) {
     /**
      * set description for description meta tag
      *
      * @param string $description
      * @return void
      */
-    function DomiSetDescription(string $description): void
+    function DomiDescription(string $description): void
     {
         Domi::setDescription($description);
     }
 }
 
-if (!function_exists('DomiSetKeywords')) {
+if (!function_exists('DomiKeywords')) {
     /**
      * set keywords for keywords meta tag
      *
      * @param string $keywords
      * @return void
      */
-    function DomiSetKeywords(string $keywords): void
+    function DomiKeywords(string $keywords): void
     {
         Domi::setKeywords($keywords);
     }
 }
 
-if (!function_exists('DomiSetAuthor')) {
+if (!function_exists('DomiAuthor')) {
     /**
      * set author for author meta tag
      *
      * @param string $author
      * @return void
      */
-    function DomiSetAuthor(string $author): void
+    function DomiAuthor(string $author): void
     {
         Domi::setAuthor($author);
     }
 }
 
-if (!function_exists('DomiSetCanonical')) {
+if (!function_exists('DomiCanonical')) {
     /**
      * set canonical url for canonical meta tag
      *
      * @param string $url
      * @return void
      */
-    function DomiSetCanonical(string $url): void
+    function DomiCanonical(string $url): void
     {
         Domi::setCanonical($url);
     }
 }
 
-if (!function_exists('DomiSetRobots')) {
+if (!function_exists('DomiRobots')) {
     /**
      * set robots data for robots meta tag
      *
      * @param string $robots
      * @return void
      */
-    function DomiSetRobots(string $robots): void
+    function DomiRobots(string $robots): void
     {
         Domi::setRobots($robots);
     }
 }
 
-if (!function_exists('DomiSetLink')) {
+if (!function_exists('DomiLink')) {
     /**
      * set link data for link tag
      *
@@ -89,13 +89,13 @@ if (!function_exists('DomiSetLink')) {
      * @param string $rel
      * @return void
      */
-    function DomiSetLink(string $link, string $rel): void
+    function DomiLink(string $link, string $rel): void
     {
         Domi::setLink($link, $rel);
     }
 }
 
-if (!function_exists('DomiSetStyle')) {
+if (!function_exists('DomiStyle')) {
     /**
      * set style data for style tag
      *
@@ -104,13 +104,13 @@ if (!function_exists('DomiSetStyle')) {
      * @param string|null $media
      * @return void
      */
-    function DomiSetStyle(string $href, string $rel = 'stylesheet', string $media = null): void
+    function DomiStyle(string $href, string $rel = 'stylesheet', string $media = null): void
     {
         Domi::setStyle($href, $rel, $media);
     }
 }
 
-if (!function_exists('DomiSetScript')) {
+if (!function_exists('DomiScript')) {
     /**
      * set script data for script tag
      *
@@ -120,13 +120,13 @@ if (!function_exists('DomiSetScript')) {
      * @param boolean $defer
      * @return void
      */
-    function DomiSetScript(string $src, string $type = 'application/javascript', bool $async = false, bool $defer = false): void
+    function DomiScript(string $src, string $type = 'application/javascript', bool $async = false, bool $defer = false): void
     {
         Domi::setScript($src, $type, $async, $defer);
     }
 }
 
-if (!function_exists('DomiSetLocalize')) {
+if (!function_exists('DomiLocalize')) {
     /**
      * set localize data for localize script variable
      *
@@ -134,85 +134,85 @@ if (!function_exists('DomiSetLocalize')) {
      * @param array $l10n
      * @return void
      */
-    function DomiSetLocalize(string $key = null, array $l10n = []): void
+    function DomiLocalize(string $key = null, array $l10n = []): void
     {
         Domi::setLocalize($key, $l10n);
     }
 }
 
-if (!function_exists('DomiSetPlugins')) {
+if (!function_exists('DomiPlugins')) {
     /**
      * set plugins by parameters
      *
      * @param mixed ...$parameters
      * @return void
      */
-    function DomiSetPlugins(...$parameters): void
+    function DomiPlugins(...$parameters): void
     {
         Domi::setPlugins($parameters);
     }
 }
 
-if (!function_exists('DomiSetTemplate')) {
+if (!function_exists('DomiTemplate')) {
     /**
      * set template name
      *
      * @param string $template
      * @return void
      */
-    function DomiSetTemplate(string $template): void
+    function DomiTemplate(string $template): void
     {
         Domi::setTemplate($template);
     }
 }
 
-if (!function_exists('DomiSetLogo')) {
+if (!function_exists('DomiLogo')) {
     /**
      * set logo path
      *
      * @param string $logo
      * @return void
      */
-    function DomiSetLogo(string $logo): void
+    function DomiLogo(string $logo): void
     {
         Domi::setLogo($logo);
     }
 }
 
-if (!function_exists('DomiSetFavicon')) {
+if (!function_exists('DomiFavicon')) {
     /**
      * set favicon path
      *
      * @param string $favicon
      * @return void
      */
-    function DomiSetFavicon(string $favicon): void
+    function DomiFavicon(string $favicon): void
     {
         Domi::setFavicon($favicon);
     }
 }
 
-if (!function_exists('DomiSetThemeColor')) {
+if (!function_exists('DomiThemeColor')) {
     /**
      * set theme color for theme-color meta tag in mobile browsers
      *
      * @param string $color
      * @return void
      */
-    function DomiSetThemeColor(string $color): void
+    function DomiThemeColor(string $color): void
     {
         Domi::setThemeColor($color);
     }
 }
 
-if (!function_exists('DomiSetPageType')) {
+if (!function_exists('DomiPageType')) {
     /**
      * set page type for og:type meta tag
      * @param string $type
      * @return void
      * @see PageTypeEnum
      */
-    function DomiSetPageType(string $type = 'website'): void
+    function DomiPageType(string $type = 'website'): void
     {
         Domi::setPageType($type);
     }
