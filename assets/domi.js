6b8bb62cbecf5360cfa27fe1d5ef2f575a24db5e
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-30 18:19:11 +0330

    get csrf token function in domi script

--- assets/domi.js
+++ assets/domi.js
@@ -2,3 +2,4 @@
 
 let keyExistsOn = (o, k) => k.split(".").reduce((a, c) => a.hasOwnProperty(c) ? a[c] || 1 : false, Object.assign({}, o)) !== false;
 let getLocalize = (key) => keyExistsOn(localize, key) ? eval("localize." + key) : undefined;
+let getCsrfToken = () => document.querySelector("meta[name=csrf-token]").getAttribute("content");
