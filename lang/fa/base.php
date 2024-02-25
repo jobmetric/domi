00aa6ac42dc9dc1395b1189ff54d9adddecf443c
majid mohammadian <majeedmohammadian@gmail.com>
2024-02-25 14:22:49 +0330

    add language fa

---
+++ lang/fa/base.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Domi Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Domi for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'direction' => 'rtl',
+    'lang' => 'fa',
+
+];
---
+++ lang/fa/datatable.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Datatable Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'processing'        => 'در حال بارگذاری...',
+    'search'            => 'جستجو: ',
+    'lengthMenu'        => 'شماره : _MENU_',
+    'info'              => 'نمایش _START_ تا _END_ از _TOTAL_',
+    'infoEmpty'         => 'آیتمی برای نمایش وجود ندارد',
+    'infoFiltered'      => '',
+    'infoPostFix'       => '',
+    'loadingRecords'    => 'در حال بارگذاری...',
+    'zeroRecords'       => 'جستجو نتیجه‌ای نداشت',
+    'emptyTable'        => 'جدول خالی است',
+    'paginate_first'    => '<<',
+    'paginate_previous' => '<',
+    'paginate_next'     => '>',
+    'paginate_last'     => '>>'
+
+];
