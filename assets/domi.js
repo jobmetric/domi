de3df5b3091ce316ecc52ddb4b72e529f58017d9
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-21 19:58:42 +0330

    add convert rem to pixels function in domi script

--- assets/domi.js
+++ assets/domi.js
@@ -1,5 +1,6 @@
 "use strict";
 
-let keyExistsOn = (o, k) => k.split(".").reduce((a, c) => a.hasOwnProperty(c) ? a[c] || 1 : false, Object.assign({}, o)) !== false;
-let getLocalize = (key) => keyExistsOn(localize, key) ? eval("localize." + key) : undefined;
-let getCsrfToken = () => document.querySelector("meta[name=csrf-token]").getAttribute("content");
+let keyExistsOn = (o, k) => k.split(".").reduce((a, c) => a.hasOwnProperty(c) ? a[c] || 1 : false, Object.assign({}, o)) !== false
+let getLocalize = (key) => keyExistsOn(localize, key) ? eval("localize." + key) : undefined
+let getCsrfToken = () => document.querySelector("meta[name=csrf-token]").getAttribute("content")
+let convertRemToPixels = (r) => r * parseFloat(getComputedStyle(document.documentElement).fontSize)
