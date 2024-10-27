8ae97c4d0452eb20b5cb0d2c5d2203877d50fbc3
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-28 00:53:31 +0330

    added modal centered in render modal

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -1,6 +1,6 @@
 @foreach(\JobMetric\Domi\Facades\Domi::modal() as $modal_id => $modal_value)
     <div class="modal fade" id="modal-{{ $modal_id }}" tabindex="-1" aria-labelledby="modal_{{ $modal_id }}_label" aria-hidden="true">
-        <div class="modal-dialog {{ $modal_value['scrollable'] }} {{ $modal_value['size'] }} {{ $modal_value['fullscreen'] }}">
+        <div class="modal-dialog {{ $modal_value['scrollable'] }} {{ $modal_value['centered'] }} {{ $modal_value['size'] }} {{ $modal_value['fullscreen'] }}">
             <div class="modal-content">
                 @if (!is_null($modal_value['header']))
                     <div class="modal-header py-4">{!! $modal_value['header'] !!}</div>
